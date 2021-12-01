80d088c38f2a1edb334ee062f7237865e3947921
Nick <nickisnick@gmail.com>
2021-11-30 19:01:59 -0500

    new pattern ala JT

--- test/test.ino
+++ test/test.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 
-typedef void (* GenericFP)();
+typedef void (*GenericFP)();
 
 #define NUMLEDS 29
 #define DATAPIN 2
@@ -8,7 +8,9 @@ typedef void (* GenericFP)();
 CRGB leds[NUMLEDS];
 CRGB randomColors[] = {CRGB::Red, CRGB::Green, CRGB::Indigo, CRGB::Blue, CRGB::Orange, CRGB::Yellow, CRGB::Violet};
 int bells[] = {4, 8, 12, 16, 21, 24};
-GenericFP patterns[] = {&allLightsOneRandomColor, &walkColors, &randomizeAllLights};
+int lightPairs1[] = {0, 1, 2, 3, 9, 10, 11, 17, 18, 19, 25, 26, 27, 28};
+int lightPairs2[] = {5, 6, 7, 13, 14, 15, 21, 22, 23};
+GenericFP patterns[] = {&alternateLightPairs, &allLightsOneRandomColor, &walkColors, &randomizeAllLights};
 int patternFunction = 0;
 int patternLoop = 1;
 
@@ -21,10 +23,11 @@ void setup()
 
   FastLED.addLeds<WS2811, DATAPIN, RGB>(leds, NUMLEDS);
   FastLED.clear();
-  
+
   for (int i = 0; i < NUMLEDS; i++)
   {
-    if(isBell(i)){
+    if (isBell(i))
+    {
       leds[i] = CRGB::Black; // WHite
     }
   }
@@ -51,45 +54,92 @@ void loop()
 
   patternLoop += 1;
 
-  if(patternLoop >= 5){
-    patternFunction = (patternFunction + 1)  % 3;
+  if (patternLoop >= 5)
+  {
+    patternFunction = (patternFunction + 1) % 4;
     patternLoop = 1;
   }
 }
 
-void randomizeAllLights(){
+void randomizeAllLights()
+{
   for (int i = 0; i < NUMLEDS; i++)
   {
-    if(!isBell(i)){
+    if (!isBell(i))
+    {
       leds[i] = randomColor();
     }
   }
 }
 
-void allLightsOneRandomColor(){
+void alternateLightPairs()
+{
+  CRGB color1 = randomColor();
+  CRGB color2 = randomColor();
+  int lightPair1Length = sizeof(lightPairs1) / sizeof(int);
+  int lightPair2Length = sizeof(lightPairs2) / sizeof(int);
+
+  for (int j = 0; j < 5; j++)
+  {
+    for (int i = 0; i < lightPair1Length; i++)
+    {
+      leds[lightPairs1[i]] = color1;
+    }
+
+    for (int i = 0; i < lightPair2Length; i++)
+    {
+      leds[lightPairs2[i]] = CRGB::Black;
+    }
+
+    FastLED.show();
+
+    delay(500);
+
+    for (int i = 0; i < lightPair1Length; i++)
+    {
+      leds[lightPairs1[i]] = CRGB::Black;
+    }
+
+    for (int i = 0; i < lightPair2Length; i++)
+    {
+      leds[lightPairs2[i]] = color2;
+    }
+
+    FastLED.show();
+
+    delay(500);
+  }
+}
+
+void allLightsOneRandomColor()
+{
   CRGB color = randomColor();
 
   for (int i = 0; i < NUMLEDS; i++)
   {
-    if(!isBell(i)){
+    if (!isBell(i))
+    {
       leds[i] = color;
     }
   }
 }
 
-void walkColors(){
+void walkColors()
+{
   for (int i = 0; i < NUMLEDS; i++)
   {
-    if(!isBell(i)){
-      leds[i] = CRGB(0,0,0); // turn all leds off
+    if (!isBell(i))
+    {
+      leds[i] = CRGB(0, 0, 0); // turn all leds off
     }
   }
 
   FastLED.show();
-  
+
   for (int i = 0; i < NUMLEDS; i++)
   {
-    if(!isBell(i)){
+    if (!isBell(i))
+    {
       leds[i] = randomColor();
       FastLED.show();
       delay(100);
@@ -98,7 +148,7 @@ void walkColors(){
 
   for (int i = NUMLEDS - 1; i >= 0; i--)
   {
-    leds[i] = CRGB(0,0,0);
+    leds[i] = CRGB(0, 0, 0);
     FastLED.show();
     delay(100);
   }
@@ -114,10 +164,12 @@ void blinkInternalLight()
   delay(x);                        // wait for a second
 }
 
-CRGB randomColor(){
+CRGB randomColor()
+{
   return randomColors[random(0, 7)];
 }
 
-bool isBell(int value){
+bool isBell(int value)
+{
   return value == 4 || value == 8 || value == 12 || value == 16 || value == 20 || value == 24;
 }
